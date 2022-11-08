c87235a55f33c93d6da7ca02a69a4e614d1ef108
Dominik Jones <dominikjo22@gmail.com>
2022-11-08 01:48:10 -0500

    updates

--- server.js
+++ server.js
@@ -1,93 +1,93 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { response } = require('express')
-const { request } = require('http')
 const PORT = 8000
 
 app.use(cors())
 
-const char = {
-    'invincible ': {
-        'charName' : 'Mark Grayson',
-        'speciesName' : 'Viltrumite-Human hybrid',
-        'homeWorld' : 'Earth',
-        'Team' : 'Teen Team and Guardians of the Globe',
-        'Suit' : 'Yellow, Blue, Black',
-        'Abilities' : 'Superhuman Strength, Stamina, Speed, Invulnerability, Flight, Enhanced Healing Factor,Decelerated aging',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/a/a3/Invincible_%28Mark_Grayson%29.png/revision/latest/scale-to-width-down/333?cb=20210328173918',
-    },
+const char = {'invincible ': {
+    'charName' : 'Mark Grayson',
+    'speciesName' : 'Viltrumite-Human hybrid',
+    'homeWorld' : 'Earth',
+    'Team' : 'Teen Team and Guardians of the Globe',
+    'Suit' : 'Yellow, Blue, Black',
+    'Abilities' : 'Superhuman Strength, Stamina, Speed, Invulnerability, Flight, Enhanced Healing Factor,Decelerated aging',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/a/a3/Invincible_%28Mark_Grayson%29.png/revision/latest/scale-to-width-down/333?cb=20210328173918',
+},
 
-    'omni-man': {
-        'charName' : 'Nolan Grayson',
-        'speciesName' : 'Viltrumite',
-        'homeWorld' : 'Viltrum',
-        'Team' : 'Guardians of the Globe and Viltrum Empire',
-        'Suit' : 'Red and White',
-        'Abilities' : 'Superhuman Strength, Stamina, Speed, Invulnerability, Flight, Enhanced Healing Factor,Decelerated aging',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/6/63/Omni-ManProfile.png/revision/latest?cb=20210619064910',
-    },
+'omni-man': {
+    'charName' : 'Nolan Grayson',
+    'speciesName' : 'Viltrumite',
+    'homeWorld' : 'Viltrum',
+    'Team' : 'Guardians of the Globe and Viltrum Empire',
+    'Suit' : 'Red and White',
+    'Abilities' : 'Superhuman Strength, Stamina, Speed, Invulnerability, Flight, Enhanced Healing Factor,Decelerated aging',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/6/63/Omni-ManProfile.png/revision/latest?cb=20210619064910',
+},
 
-    'battle beast': {
-        'charName' : 'Thokk',
-        'speciesName' : 'Feline',
-        'homeWorld' : 'Thraxa',
-        'Team' : 'None',
-        'Suit' : 'Red and Black',
-        'Abilities' : 'Super Strength, Infinite Stamina, Hand to Hand Combat, Weapons',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/4/41/Battle_Beast_Ep_5_Invincible_%28244%29.png/revision/latest/scale-to-width-down/350?cb=20210409145922',
-    },
-    
-    'atom eve':{
-        'charName' : 'Sam Wilkins',
-        'speciesName' : 'Human',
-        'homeWorld' : 'Earth',
-        'Team' : 'Teen Team',
-        'Suit' : 'Pink',
-        'Abilities' : 'Subatomic Manipulation, Energy Manipulation, Flight',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/7/74/Atom-EveProfile.png/revision/latest/scale-to-width-down/246?cb=20210619065420',
-    },
+'battle beast': {
+    'charName' : 'Thokk',
+    'speciesName' : 'Feline',
+    'homeWorld' : 'Thraxa',
+    'Team' : 'None',
+    'Suit' : 'Red and Black',
+    'Abilities' : 'Super Strength, Infinite Stamina, Hand to Hand Combat, Weapons',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/4/41/Battle_Beast_Ep_5_Invincible_%28244%29.png/revision/latest/scale-to-width-down/350?cb=20210409145922',
+},
 
-    'cecil stedman': {
-        'charName' : 'Cecil Stedman',
-        'speciesName' : 'Human',
-        'homeWorld' : 'Earth',
-        'Team' : 'Global Defense Agency',
-        'Suit' : 'Black, White, Red',
-        'Abilities' : 'The US Military',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/f/f1/CecilProfile.png/revision/latest/scale-to-width-down/229?cb=20210619072853',
-    },
+'atom eve':{
+    'charName' : 'Sam Wilkins',
+    'speciesName' : 'Human',
+    'homeWorld' : 'Earth',
+    'Team' : 'Teen Team',
+    'Suit' : 'Pink',
+    'Abilities' : 'Subatomic Manipulation, Energy Manipulation, Flight',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/7/74/Atom-EveProfile.png/revision/latest/scale-to-width-down/246?cb=20210619065420',
+},
 
-    'allen the alien': {
-        'charName' : 'Allen',
-        'speciesName' : 'Unopian',
-        'homeWorld' : 'Unknown',
-        'Team' : 'Coalition of Planets',
-        'Suit' : 'Purple and White',
-        'Abilities' : 'Super Strength, Durability, Telepathy, Flight',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/9/94/Allen_The_Aliens_s448_%281%29.png/revision/latest/scale-to-width-down/350?cb=20210327224202',
-    },
+'cecil stedman': {
+    'charName' : 'Cecil Stedman',
+    'speciesName' : 'Human',
+    'homeWorld' : 'Earth',
+    'Team' : 'Global Defense Agency',
+    'Suit' : 'Black, White, Red',
+    'Abilities' : 'The US Military',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/f/f1/CecilProfile.png/revision/latest/scale-to-width-down/229?cb=20210619072853',
+},
 
-    'rex splode': {
-        'charName' : 'Rex Sloane',
-        'speciesName' : 'Human',
-        'homeWorld' : 'Earth',
-        'Team' : 'Teen Team and Guardians of the Globe',
-        'Suit' : 'Red and Yellow',
-        'Abilities' : 'Enhanced Druability, Energy Projection, Agility',
-        'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/8/87/Rex-SplodeProfile.png/revision/latest/scale-to-width-down/199?cb=20210619071518',
-    },
-    
-    'unknown': {
-        'charName' : 'NA',
-        'speciesName' : 'NA',
-        'homeWorld' : 'NA',
-        'Team' : 'NA',
-        'Suit' : 'NA',
-        'Abilities' : 'NA',
-        'image' : 'NA',
-    }
+'allen the alien': {
+    'charName' : 'Allen',
+    'speciesName' : 'Unopian',
+    'homeWorld' : 'Unknown',
+    'Team' : 'Coalition of Planets',
+    'Suit' : 'Purple and White',
+    'Abilities' : 'Super Strength, Durability, Telepathy, Flight',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/9/94/Allen_The_Aliens_s448_%281%29.png/revision/latest/scale-to-width-down/350?cb=20210327224202',
+},
+
+'rex splode': {
+    'charName' : 'Rex Sloane',
+    'speciesName' : 'Human',
+    'homeWorld' : 'Earth',
+    'Team' : 'Teen Team and Guardians of the Globe',
+    'Suit' : 'Red and Yellow',
+    'Abilities' : 'Enhanced Druability, Energy Projection, Agility',
+    'image' : 'https://static.wikia.nocookie.net/amazon-invincible/images/8/87/Rex-SplodeProfile.png/revision/latest/scale-to-width-down/199?cb=20210619071518',
+},
+
+'unknown': {
+    'charName' : 'NA',
+    'speciesName' : 'NA',
+    'homeWorld' : 'NA',
+    'Team' : 'NA',
+    'Suit' : 'NA',
+    'Abilities' : 'NA',
+    'image' : 'NA',
 }
+}
+
+    
+
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
